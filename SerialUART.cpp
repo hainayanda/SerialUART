bf0123da8369d0748d62e946a72fe65ac686cdd9
Nayanda Haberty <naysone@yahoo.com>
2017-02-13 07:47:38 +0700

    first commit

---
+++ SerialUART.cpp
@@ -0,0 +1,126 @@
+﻿/*
+ * SerialUART.cpp
+ *
+ * Created: 2/12/2017 5:41:25 PM
+ * Author : Nayanda Haberrty <nayanda1@outlook.com>
+ */
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include "SerialUART2560.h"
+
+SerialUART::SerialUART(uint8_t *ubrrl, uint8_t *ubrrh,  uint8_t *ucsra,  uint8_t *ucsrb, uint8_t *ucsrc, uint8_t *udr){
+	init(9600L, ubrrl, ubrrh, ucsra, ucsrb, ucsrc, udr);
+}
+
+SerialUART::SerialUART(unsigned long baud, uint8_t *ubrrl, uint8_t *ubrrh,  uint8_t *ucsra,  uint8_t *ucsrb, uint8_t *ucsrc, uint8_t *udr){
+	init(baud, ubrrl, ubrrh, ucsra, ucsrb, ucsrc, udr);
+}
+
+SerialUART::~SerialUART(void){
+	flush();
+	*ubrrh = 0x00;
+	*ubrrl = 0x00;
+	*ucsra = 0x00;
+	*ucsrb = 0x00;
+	*ucsrc = 0x00;
+}
+
+void SerialUART::init(unsigned long baud, uint8_t *ubrrl, uint8_t *ubrrh,  uint8_t *ucsra,  uint8_t *ucsrb, uint8_t *ucsrc, uint8_t *udr){
+	SerialUART::init(this, baud, ubrrl, ubrrh, ucsra, ucsrb, ucsrc, udr);
+}
+
+void SerialUART::init(SerialUART *serial, unsigned long baud, uint8_t *ubrrl, uint8_t *ubrrh,  uint8_t *ucsra, uint8_t *ucsrb, uint8_t *ucsrc,  uint8_t *udr){
+	serial->timeOneBit = 1000000/baud;
+	serial->ubrrVal = F_CPU/16/baud-1;
+	serial->ubrrl = ubrrl;
+	serial->ubrrh = ubrrh;
+	serial->ucsra = ucsra;
+	serial->ucsrb = ucsrb;
+	serial->ucsrc = ucsrc;
+	serial->udr = udr;
+	*(serial->ubrrh) = serial->ubrrVal>>7;
+	*(serial->ubrrl) = serial->ubrrVal;
+	 //enable TX0 and RX0
+	*(serial->ucsrb) = ((1<<RX_ENABLE)|(1<<TX_ENABLE));
+	 // set 8 bit data and 2 bit stop bit
+	*(serial->ucsrc) = ((1<<U_STOP_BIT)|(1<<UCSZ0)|(1<<UCSZ1));
+}
+
+char SerialUART::getChar(void){
+	return SerialUART::getChar(ucsra, udr);
+}
+
+char SerialUART::getChar(volatile uint8_t *ucsra, volatile uint8_t *udr){
+	while (!(*ucsra & (1<<RX_FLAG)));
+	return *udr;
+}
+
+void SerialUART::putChar(const char character){
+	SerialUART::putChar(character);
+}
+
+void SerialUART::putChar(volatile uint8_t *ucsra, volatile uint8_t *udr, const char character){
+	while (!(*ucsra & (1<<UDR_READY)));
+	*udr = character;
+}
+
+void SerialUART::print(const char *string){
+	SerialUART::print(string);
+}
+
+void SerialUART::print(SerialUART *uart, char *string){
+	while(*string != '\0'){
+		uart->putChar(*string);
+		string++;
+	}
+}
+
+void SerialUART::scan(char *str, unsigned int size){
+	SerialUART::scan(this, str, size);
+}
+
+void SerialUART::scan(SerialUART *uart, char *str, unsigned int size){
+	unsigned int i = 0, t = 0;
+	while(!t && i < size){
+		*str = uart->getChar();
+		t = uart->timeout();
+		str++; i++;
+	}
+}
+
+bool SerialUART::available(void){
+	return SerialUART::available(ucsra);
+}
+
+bool SerialUART::available(volatile uint8_t *ucsra){
+	if (*ucsra & (1<<RX_FLAG)) return true;
+	else return false;
+}
+
+bool SerialUART::timeout(void){
+	return SerialUART::timeout(this);
+}
+
+bool SerialUART::timeout(SerialUART *uart){
+	int time = 0;
+	while (!uart->available() && time < 1000){
+		unsigned int i= 0;
+		while(i < uart->timeOneBit){
+			_delay_us(1);
+			i++;
+		}
+		time++;
+	} if (time < 1000) return false;
+	else return true;
+}
+
+void SerialUART::flush(void){
+	SerialUART::flush(ucsra, udr);
+}
+
+void SerialUART::flush(volatile uint8_t *ucsra, volatile uint8_t *udr){
+	char dummy;
+	while (*ucsra & (1<<RX_FLAG)) dummy = *udr;
+	return;
+}
---
+++ SerialUART.h
@@ -0,0 +1,237 @@
+/*
+ * SerialUART.h
+ *
+ * Created: 2/12/2017 5:41:25 PM
+ * Author : Nayanda Haberrty <nayanda1@outlook.com>
+ */ 
+ 
+ #ifndef UARTSERIAL2560_H_
+#define UARTSERIAL2560_H_
+#define __DELAY_BACKWARD_COMPATIBLE__
+
+#endif /* UARTSERIAL2560_H_ */
+
+#ifndef F_CPU
+#define F_CPU 16000000UL
+#endif
+
+class SerialUART{
+	private:
+
+	static const uint8_t RX_ENABLE = 4, TX_ENABLE = 3, U_STOP_BIT = 3, UCSZ0 = 1, UCSZ1 = 2, RX_FLAG = 7, UDR_READY = 5;
+
+	protected:
+
+	//register address
+	volatile uint8_t *ubrrl, *ubrrh, *ucsra, *ucsrb, *ucsrc, *udr;
+
+	//class variable
+	uint16_t ubrrVal;
+	unsigned long timeOneBit;
+
+	/**
+	 * \brief 
+	 * 
+	 * \param 
+	 * 
+	 * \return bool
+	 */
+	bool timeout(void);
+
+	/**
+	 * \brief 
+	 * 
+	 * \param serial
+	 * 
+	 * \return bool
+	 */
+	static bool timeout(SerialUART *serial);
+
+	/**
+	 * \brief 
+	 * 
+	 * \param serial
+	 * \param baud
+	 * \param ubrrl
+	 * \param ubrrh
+	 * \param ucsra
+	 * \param ucsrb
+	 * \param ucsrc
+	 * \param udr
+	 * 
+	 * \return void
+	 */
+	static void init(SerialUART *serial, unsigned long baud, uint8_t *ubrrl, uint8_t *ubrrh, uint8_t *ucsra, uint8_t *ucsrb, uint8_t *ucsrc, uint8_t *udr);
+	
+	/**
+	 * \brief 
+	 * 
+	 * \param ucsra
+	 * \param udr
+	 * 
+	 * \return char
+	 */
+	static char getChar(volatile uint8_t *ucsra, volatile uint8_t *udr);
+	
+	/**
+	 * \brief 
+	 * 
+	 * \param ucsra
+	 * \param udr
+	 * \param character
+	 * 
+	 * \return void
+	 */
+	static void putChar(volatile uint8_t *ucsra, volatile uint8_t *udr, const char character);
+	
+	/**
+	 * \brief 
+	 * 
+	 * \param serial
+	 * \param string
+	 * 
+	 * \return void
+	 */
+	static void print(SerialUART *serial, char *string);
+	
+	/**
+	 * \brief 
+	 * 
+	 * \param serial
+	 * \param string
+	 * \param size
+	 * 
+	 * \return void
+	 */
+	static void scan(SerialUART *serial, char *string, unsigned int size);
+	
+	/**
+	 * \brief 
+	 * 
+	 * \param ucsra
+	 * 
+	 * \return bool
+	 */
+	static bool available(volatile uint8_t *ucsra);
+	
+	/**
+	 * \brief 
+	 * 
+	 * \param ucsra
+	 * \param udr
+	 * 
+	 * \return void
+	 */
+	static void flush(volatile uint8_t *ucsra, volatile uint8_t *udr);
+
+	public:
+	
+	/**
+	 * \brief 
+	 * 
+	 * \param ubrrl
+	 * \param ubrrh
+	 * \param ucsra
+	 * \param ucsrb
+	 * \param ucsrc
+	 * \param udr
+	 * 
+	 * \return 
+	 */
+	SerialUART(uint8_t *ubrrl, uint8_t *ubrrh, uint8_t *ucsra, uint8_t *ucsrb, uint8_t *ucsrc, uint8_t *udr);
+	
+	/**
+	 * \brief 
+	 * 
+	 * \param baud
+	 * \param ubrrl
+	 * \param ubrrh
+	 * \param ucsra
+	 * \param ucsrb
+	 * \param ucsrc
+	 * \param udr
+	 * 
+	 * \return 
+	 */
+	SerialUART(unsigned long baud, uint8_t *ubrrl, uint8_t *ubrrh, uint8_t *ucsra, uint8_t *ucsrb, uint8_t *ucsrc, uint8_t *udr);
+	
+	/**
+	 * \brief 
+	 * 
+	 * \param 
+	 * 
+	 * \return 
+	 */
+	~SerialUART(void);
+
+	/**
+	 * \brief 
+	 * 
+	 * \param baud
+	 * \param ubrrl
+	 * \param ubrrh
+	 * \param ucsra
+	 * \param ucsrb
+	 * \param ucsrc
+	 * \param udr
+	 * 
+	 * \return void
+	 */
+	void init(unsigned long baud, uint8_t *ubrrl, uint8_t *ubrrh, uint8_t *ucsra, uint8_t *ucsrb, uint8_t *ucsrc, uint8_t *udr);
+	
+	/**
+	 * \brief 
+	 * 
+	 * \param 
+	 * 
+	 * \return char
+	 */
+	char getChar(void);
+	
+	/**
+	 * \brief 
+	 * 
+	 * \param character
+	 * 
+	 * \return void
+	 */
+	void putChar(char character);
+	
+	/**
+	 * \brief 
+	 * 
+	 * \param string
+	 * 
+	 * \return void
+	 */
+	void print(const char *string);
+	
+	/**
+	 * \brief 
+	 * 
+	 * \param string
+	 * \param size
+	 * 
+	 * \return void
+	 */
+	void scan(char *string, unsigned int size);
+	
+	/**
+	 * \brief 
+	 * 
+	 * \param 
+	 * 
+	 * \return bool
+	 */
+	bool available(void);
+	
+	/**
+	 * \brief 
+	 * 
+	 * \param 
+	 * 
+	 * \return void
+	 */
+	void flush(void);
+
+};
